0f3251817403746ebbfcf13845eaed7f9799b126
Karannn23 <93504042+Karannn23@users.noreply.github.com>
2022-09-05 23:24:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.getElementById("login-button").addEventListener("click",function(){
   let log_id = document.getElementById("input-id").value;
   let log_pswd = document.getElementById("input-pswd").value;
-  if(log_id == "shirazhambi786@gmail.com" && log_pswd == "Passw@123"){
+  if(log_id == "karansingh.k12345@gmail.com" && log_pswd == "Passw@123"){
     console.log(log_id);
     console.log(log_pswd);
 
